6409c03b3bc3237eb05e775cb71114ca85ec1a21
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2025-11-28 22:31:17 -0600

    Add loadFonts to Captcha mock to fix server init crash

--- rsc-cloudflare/rsc-server/src/captcha-mock.js
+++ rsc-cloudflare/rsc-server/src/captcha-mock.js
@@ -10,6 +10,11 @@ class Captcha {
             data: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==' // 1x1 transparent pixel
         };
     }
+
+    async loadFonts() {
+        console.log('Captcha.loadFonts() called (mock)');
+        return Promise.resolve();
+    }
 }
 
 module.exports = Captcha;
