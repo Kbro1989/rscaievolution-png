17b37ad33a8819b4362df58bbf964ede8e4a7ba2
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2025-11-28 22:14:05 -0600

    Mock rsc-captcha in browser build to prevent canvas crash

---
+++ rsc-cloudflare/rsc-server/src/captcha-mock.js
@@ -0,0 +1,15 @@
+class Captcha {
+    constructor() {
+        console.log('Captcha mock initialized');
+    }
+
+    async generate() {
+        console.log('Captcha.generate() called (mock)');
+        return {
+            text: '00000',
+            data: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==' // 1x1 transparent pixel
+        };
+    }
+}
+
+module.exports = Captcha;
