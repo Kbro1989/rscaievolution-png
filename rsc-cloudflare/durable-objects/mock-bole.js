bacd5c6d7eb3f2f9970f8dcb29fec622aa3af51a
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2026-02-04 18:00:04 -0600

    chore: remove a file.

--- rsc-cloudflare/durable-objects/mock-bole.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (name) {
-    return {
-        info: console.log,
-        error: console.error,
-        warn: console.warn,
-        debug: console.log,
-        add: () => { }
-    };
-};
